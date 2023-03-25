0055e18905a44effd41b2ed3f79ae9f6efa9eb35
victor <viktortoro240@gmail.com>
2023-03-25 09:07:10 -0500

    version actualizada

--- views/insertarAsignaturas.php
+++ views/insertarAsignaturas.php
@@ -1,13 +1,18 @@
 <form method="post" action="#"> 
     <input type="text" name="asiganaturas" placeholder="asiganaturas">
+    <input type="number" name="fkdocente" placeholder="fkdocente">
+    <input type="number" name="fkhorario" placeholder="fkhorario">
     <input type="submit" value="enviar">
 </form>
 <?php 
 require("models/conexion.php");
 @$materias = $_POST['asiganaturas'];
+@$fkdocente = $_POST['fkdocente'];
+@$fkhorario = $_POST['fkhorario'];
+
         
-if($materias){
-    $query=mysqli_query($con,"INSERT INTO asignaturas(nombre) VALUES ('$materias')") OR DIE ("Error al realizar consulta" .mysqli_error($con));
+if($materias or $fkdocente or $fkhorario){
+    $query=mysqli_query($con,"INSERT INTO `asignaturas` (`nombre`, `fkdocente`, `fkhorario`) VALUES ('$materias', $fkdocente, $fkhorarios)") OR DIE ("Error al realizar consulta" .mysqli_error($con));
     
 }else{
     echo $materias;
